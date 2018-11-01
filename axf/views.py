4382e0de0c0a92da6e48deae16cefaf4dae0cef5
FriendA233 <1932814918@qq.com>
2018-11-01 02:30:00 -0700

    项目基本框架搭建完成

--- axf/views.py
+++ axf/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from axf.models import Wheel
+
+
 def home(request):     #首页
-    return render(request,'home/home.html')
+    #轮播图数据
+    wheels = Wheel.objects.all()
+
+    return render(request,'home/home.html',context={'wheels':wheels})
 
 
 def market(request):    #超市
-    return None
+    return render(request,'market/market.html')
 
 
 def cart(request):     #购物车
-    return None
+    return render(request,'cart/cart.html')
 
 
 def mine(request):      #我的
-    return None
+    return render(request,'mine/mine.html')
